8fcd332a7b772c8a59e194f2123024c7d827a306
Junaid Ahmed <junaid@acalvio.com>
2021-04-28 12:22:40 +0530

    registering user complete

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,42 @@
 const express = require("express");
+const {check, validationResult} = require("express-validator/check");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
 // @route       POST  api/users
 // @desc        register a user
 // @access      public
-router.post("/", (req, res)=>{
-    res.json({"msg":"register"})
+router.post("/", [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "email is required").isEmail(),
+    check("password", "password is required").isLength({min:6})
+    ], async (req, res)=>{
+
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({"errors": errors.array()});
+        }else{
+            const {name, email, password} = req.body;
+            try{
+                let user = await User.findOne({email: email});
+                if(user){
+                    return res.status(400).json({msg: "user already exists"});
+                }
+                user = new User({
+                    name,email,password
+                });
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+                await user.save();
+                return res.send("user saved")
+            }catch(err){
+                console.log(err.message);
+                return res.status(500).send({err: "server error"});
+            }
+        }
+   
 })
 
 
