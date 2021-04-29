ac75b322f7dd78c75dcc98198fb6cd04b36d0dd5
Junaid Ahmed <junaid@acalvio.com>
2021-04-29 12:25:16 +0530

    user contact update adn delete api

--- routes/contacts.js
+++ routes/contacts.js
@@ -54,15 +54,47 @@ async (req, res)=>{
 // @route       PUT  api/contacts/:id
 // @desc        edit contacts
 // @access      private
-router.put("/:id", (req, res)=>{
-    res.json({"msg":"edit contact"});
+router.put("/:id",auth, async (req, res)=>{
+    const {name, email, phone, type} = req.body;
+
+    const contactFields = {};
+    if(name) contactFields.name = name;
+    if(email) contactFields.email = email;
+    if(phone) contactFields.phone = phone;
+    if(type) contactFields.type = type;
+    // contactFields.user = req.body.user.id;
+    const updatedContact = new Contact(contactFields);
+    try {
+        let contacts = await Contact.findById(req.params.id);
+        if(!contacts) return res.status(400).json({msg:"No contact found for the user"});
+
+        if(contacts.user.toString() != req.body.user.id) return res.status(401).json({msg:"Not authorized"});
+
+        let cont = await Contact.findByIdAndUpdate(req.params.id, {$set: contactFields}, {new:true});
+        return res.status(200).json(cont);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({msg:"server error"})
+    }
 });
 
 // @route       DELETE  api/contacts/:id
 // @desc        delete contacts
 // @access      private
-router.delete("/:id", (req, res)=>{
-    res.json({"msg":"delete contact"});
+router.delete("/:id",auth, async (req, res)=>{
+    try {
+        let contact = await Contact.findById(req.params.id);
+        if(!contact) return res.status(400).json({msg:"Contact not found for this user"})
+
+        if(contact.user.toString() != req.body.user.id) return res.status(400).json({msg:"Not Authorized"});
+
+        let cont = await Contact.findByIdAndRemove(req.params.id);
+        return res.status(200).json({msg:"deleted successfully"});
+
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).json({msg:"server error"});
+    }
 });
 
 
